b3b1e3e5853fb2ca2d9426f336e13ba5457590e7
Swaraj Pradhan <tts312@outlook.com>
2025-05-24 17:21:35 +0530

    [avl tree] move Node class to node header
    - move Node class to node header

--- src/avl_tree.h
+++ src/avl_tree.h
@@ -1,42 +1,6 @@
 #include<iostream>
+#include"node.h"
 using namespace std;
-
-/*
-    Node is the interface for AVLTree's node's data
-*/
-class Node {
-public:
-    virtual bool operator==(const Node &n) const =0;
-    virtual bool operator>(const Node &n) const =0;
-    virtual bool operator<(const Node &n) const =0;
-    virtual string getData() const =0;
-};
-
-/*
-    NodeImpl is an example implementation for Node interface
-*/
-class NodeImpl: public Node{
-public:
-    int val;
-    NodeImpl(int v=0): val(v) {}
-    ~NodeImpl() {}
-
-    bool operator==(const Node &n) const override {
-        const NodeImpl* ni = dynamic_cast<const NodeImpl*>(&n);
-        return val == ni->val;
-    }
-    bool operator>(const Node &n) const override {
-        const NodeImpl* ni = dynamic_cast<const NodeImpl*>(&n);
-        return val > ni->val;
-    }
-    bool operator<(const Node &n) const override {
-        const NodeImpl* ni = dynamic_cast<const NodeImpl*>(&n);
-        return val < ni->val;
-    }
-    string getData() const override {
-        return to_string(val);
-    }
-};
 
 /*
         node
